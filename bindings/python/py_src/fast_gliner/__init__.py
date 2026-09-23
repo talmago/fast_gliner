1a3805d7f02effd154194849980e1bdfcddde7b9
Tal Almagor <almagoric@gmail.com>
2026-09-23 14:05:44 +0300

    bump version to 0.3.1

--- bindings/python/py_src/fast_gliner/__init__.py
+++ bindings/python/py_src/fast_gliner/__init__.py
@@ -3,6 +3,6 @@ from .gliformer import FastGLiFormer
 from .gliner import FastGLiNER
 from .gliner2 import FastGLiNER2
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = ["FastGLiNER", "FastGLiNER2", "FastGLiClass", "FastGLiFormer"]
